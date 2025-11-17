a99a543b25bba2ccff2679d9ec6565e9313abfb2
islamashraf2003 <islamashraf520@gmail.com>
2025-11-17 04:00:56 +0200

    - added custom app theme

--- lib/core/theming/app_theme.dart
+++ lib/core/theming/app_theme.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:int_news/core/theming/colors.dart';
 
-import 'colors.dart';
+class AppTheme {
+  static const Color primaryColor = Color(0xFFF34C54);
 
-abstract class AppTheme {
-  static ThemeData lightTheme(BuildContext context) {
-    return ThemeData(
-      scaffoldBackgroundColor: ColorsManager.primaryColor,
-      appBarTheme: const AppBarTheme(
-        backgroundColor: Color.fromARGB(255, 170, 47, 47),
+  static final ThemeData lightTheme = ThemeData(
+    primaryColor: primaryColor,
+    scaffoldBackgroundColor: ColorsManager.white,
+    appBarTheme: AppBarTheme(
+      backgroundColor: primaryColor,
+      elevation: 0,
+      iconTheme: IconThemeData(color: ColorsManager.white),
+      titleTextStyle: TextStyle(
+        color: ColorsManager.white,
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
       ),
-      textSelectionTheme: const TextSelectionThemeData(
-        selectionHandleColor: ColorsManager.secendryColor,
-      ),
-      colorScheme: ColorScheme.fromSeed(seedColor: ColorsManager.secendryColor),
-      fontFamily: 'Inter',
-    );
-  }
+    ),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: primaryColor,
+    ),
+    colorScheme: ColorScheme.fromSwatch().copyWith(secondary: primaryColor),
+  );
 }
--- lib/int_news_app.dart
+++ lib/int_news_app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:int_news/core/routing/app_router.dart';
 import 'package:int_news/core/routing/routes.dart';
+import 'package:int_news/core/theming/app_theme.dart';
 
 class IntNewsApp extends StatelessWidget {
   const IntNewsApp({super.key});
@@ -9,6 +10,7 @@ class IntNewsApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
       title: 'INTnews App',
       initialRoute: Routes.homeScreen,
       onGenerateRoute: AppRouter.generateRoute,
