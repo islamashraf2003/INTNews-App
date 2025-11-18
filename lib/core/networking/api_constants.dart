5113e270a35d2a6c18a9af7b0739224c57556993
islamashraf2003 <islamashraf520@gmail.com>
2025-11-18 04:42:09 +0200

    Ignore api_constants.dart

--- lib/core/networking/api_constants.dart
+++
@@ -1,8 +0,0 @@
-class ApiConstants {
-  static const String baseUrl = "https://newsapi.org/";
-  static const String topHeadlines = "v2/top-headlines";
-  static const String everything = "v2/everything";
-  static const String sources = "v2/sources";
-
-  static const String apiKey = "c0e4f1021ef04fd094d5d0903c01f966";
-}
